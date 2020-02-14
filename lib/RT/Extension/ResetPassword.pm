fa362cc3b1ed5edb598d533ac9ce4734621a594a
sunnavy <sunnavy@bestpractical.com>
2020-02-15 05:14:14 +0800

    Update copyright to 2020

--- lib/RT/Extension/ResetPassword.pm
+++ lib/RT/Extension/ResetPassword.pm
@@ -108,7 +108,7 @@ or via the web at
 
 =head1 LICENSE AND COPYRIGHT
 
-This software is Copyright (c) 2012-2014 by Best Practical Solutions, LLC
+This software is Copyright (c) 2012-2020 by Best Practical Solutions, LLC
 
 This is free software, licensed under:
 
